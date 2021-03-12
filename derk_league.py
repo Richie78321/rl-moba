f40f486e69fc4b11a4cd278925b202c2d188de33
Matt Deible <47071205+mdeib@users.noreply.github.com>
2021-03-12 15:04:57 -0500

    Update derk_league.py

--- derk_league.py
+++ derk_league.py
@@ -8,6 +8,7 @@ import time
 import os
 from tqdm import tqdm
 from torch_truncnorm.TruncatedNormal import TruncatedNormal
+import random
 
 torch.autograd.set_detect_anomaly(True)
 
@@ -87,7 +88,7 @@ class lstm_agent(nn.Module):
         if self.use_gae:
             # lambda param for GAE estimation, defines the tradeoff between bias
             # (using the value function) and variance (using actual returns)
-            self.lamda = 0.95
+            self.lamda = 0.96
             # how much to optimize the value function, too much interferes with policy
             # leaning, too little and value function won't be accurate
             self.value_coeff = 0.5
@@ -109,7 +110,7 @@ class lstm_agent(nn.Module):
         self.eps_clip = 0.2
         # how much to optimize for entropy, prevents policy collapse by keeping some randomness for exploration
         self.entropy_coeff = 0.01
-        self.entropy_coeff_decay = 0.9997
+        self.entropy_coeff_decay = 0.9996
         # whether to treat each component of an action as independent or not.
         # by default this should be set to False
         self.independent_action_components = False
@@ -316,24 +317,13 @@ n_arenas = 800
 random_configs = [{"slots": [random.choice(arm_weapons), random.choice(misc_weapons), random.choice(tail_weapons)]} for i in range(3 * n_arenas // 2)]
 env = DerkEnv(n_arenas = n_arenas, turbo_mode = True, reward_function = win_loss_reward_function, home_team = random_configs, away_team = random_configs)
 
-save_model_every = 50
-eval_against_gap = 100
-
 league_size = 10
 teams_per_member = (env.n_agents // 3) // league_size
 league = [lstm_agent(512, device) for i in range(league_size)]
 
-model_checkpoint_schedule = [int(i ** 1.5) for i in range(1000)]
+model_checkpoint_schedule = [2*int(i ** 1.6) for i in range(1000)]
 save_folder = "checkpoints/PPO-LSTM-LEAGUE" + str(time.time())
 os.mkdir(save_folder)
-
-for i in range(100):
-    obs = env.reset()
-    print(obs)
-    while True:
-        obs, rew, done, _ = env.step(np.zeros((env.n_agents, 5)))
-        if all(done):
-            break
 
 for iteration in range(ITERATIONS):
     print("\n-----------------------------ITERATION " + str(iteration) + "-----------------------------")
@@ -359,7 +349,6 @@ for iteration in range(ITERATIONS):
         while True:
             action_n = np.zeros((env.n_agents, 5))
             for i in range(league_size):
-                print(observation_n)
                 action_n[league_agent_mappings[i]], states[i] = league[i].get_action(observation_n[league_agent_mappings[i]], states[i])
 
             #act in environment and observe the new obervation and reward (done tells you if episode is over)
