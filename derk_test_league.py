f74865398ed182ffb8b3073aa5e645f1e791695d
BryceRyan <bryceryan1999@gmail.com>
2021-03-17 13:00:55 -0400

    Added ELO support and primitive ELO scheduling

--- derk_test_league.py
+++ derk_test_league.py
@@ -72,6 +72,7 @@ class lstm_agent(nn.Module):
         super().__init__()
         self.device = device
         self.ELO=1000
+        self.id=-1
 
         #### HYPERPARAMETERS ####
 
@@ -190,10 +191,14 @@ device = "cuda:0"
 ITERATIONS = 1000000
 root_dir="checkpoints/TEST_LEAGUE_AGENTS"
 league=[]
+k=30 #ELO weight parameter
+count=0
 for root, dirs, files in os.walk(root_dir):
     for name in files: # Load in the agents stored at root_dir
         temp=lstm_agent(512, device)
         temp.load_state_dict(torch.load(os.path.join(root, name)))
+        temp.id=count
+        count+=1
         league.append(temp)
 
 league_size = len(league) # Number of policies.  Must be even because we don't want byes or anything like that.
@@ -208,7 +213,9 @@ for iteration in range(ITERATIONS):
 
     #randomize matchings between policies to start (This will eventually be more intelligent) - Can just sort by ELO and then assign that way
     #Policy index 0 plays policy index 1, 2 plays 3, and so on.
-    policy_matchup_IDS=np.random.permutation(league_size)
+    #policy_matchup_IDS=np.random.permutation(league_size)
+
+    league.sort(key=lambda x: x.ELO) # Currently sort our league based on ELO
 
     observation = [[] for i in range(league_size)]
     action = [[] for i in range(league_size)]
@@ -220,13 +227,38 @@ for iteration in range(ITERATIONS):
         while True:
             action_n = np.zeros((env.n_agents, 5)) # Array of 0's that is the right size
             for i in range(league_size): #action_n refers to an agent per index, so we assign agents sequentially to each policy based on our order config and then update
-                action_n[list(range(teams_per_member*i*3, teams_per_member*(i+1)*3))], states[i] = league[policy_matchup_IDS[i]].get_action(observation_n[list(range(teams_per_member*i*3, teams_per_member*(i+1)*3))], states[i]) # league is where the policies are stored
-
+                action_n[list(range(teams_per_member*i*3, teams_per_member*(i+1)*3))], states[i] = league[i].get_action(observation_n[list(range(teams_per_member*i*3, teams_per_member*(i+1)*3))], states[i]) # league is where the policies are stored
+            # All of a policy's agents are stored together, so we just need to sum the reward functions of each policy in the matchup and compare
             #act in environment and observe the new obervation and reward (done tells you if episode is over)
             observation_n, reward_n, done_n, _ = env.step(action_n)
 
             if all(done_n): # We have to update ELO here
-              break
+                for i in range(0, league_size, 2):
+                    policy1score=sum(reward_n[teams_per_member*i*3:teams_per_member*(i+1)*3])
+                    policy2score=sum(reward_n[teams_per_member*(i+1)*3:teams_per_member*(i+2)*3])
+
+                    policy1prob=1.0/(1.0+pow(10, (league[i].ELO-league[i+1].ELO)/400))
+                    policy2prob=1.0/(1.0+pow(10, (league[i+1].ELO-league[i].ELO)/400))
+
+                    policy1actual=0
+                    policy2actual=0
+                    
+                    
+                    if policy1score>policy2score:
+                        policy1actual=1
+                    elif policy1score<policy2score:
+                        policy2actual=1
+                    else:
+                        policy1actual=.5
+                        policy2actual=.5
+
+                    
+                    league[i].ELO+=k*(policy1actual-policy1prob)
+                    league[i+1].ELO+=k*(policy2actual-policy2prob)
+                    print(league[i].ELO)
+                    print(league[i+1].ELO)
+                    
+                break
 
 env.close()
 
