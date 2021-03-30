41654bf1800109d76610bf01b1cf451b886b2f06
Nick Zullo <njz12@pitt.edu>
2021-03-29 20:47:22 -0400

    Convert indentation to spaces

--- derk_PBT_async.py
+++ derk_PBT_async.py
@@ -68,71 +68,75 @@ hyperparam_history_save_file = os.path.join(save_folder, 'hyperparam_history.jso
 update_hyperparameter_history(hyperparam_history_save_file, list(zip(population, [True] * len(population))), 0)
 
 async def run(env: DerkSession, app: DerkAppInstance):
-	teams_per_member = (env.n_agents // 3) // population_size
-	for iteration in range(ITERATIONS):
-		print("\n-----------------------------ITERATION " + str(iteration) + "-----------------------------")
+    teams_per_member = (env.n_agents // 3) // population_size
+    for iteration in range(ITERATIONS):
+        print("\n-----------------------------ITERATION " + str(iteration) + "-----------------------------")
 
-		#randomize matchings between league members
-		scrambled_team_IDS = np.random.permutation(env.n_agents // 3)
-		league_agent_mappings = []
-		for i in range(population_size):
-			member_matches = scrambled_team_IDS[teams_per_member*i:teams_per_member*(i+1)]
-			league_agent_mappings.append(np.concatenate([(member_matches * 3) + i for i in range(3)], axis = 0))
+        #randomize matchings between league members
+        scrambled_team_IDS = np.random.permutation(env.n_agents // 3)
+        league_agent_mappings = []
+        for i in range(population_size):
+            member_matches = scrambled_team_IDS[teams_per_member*i:teams_per_member*(i+1)]
+            league_agent_mappings.append(np.concatenate([(member_matches * 3) + i for i in range(3)], axis = 0))
 
-		if iteration in model_checkpoint_schedule:
-			for i in range(population_size):
-				torch.save(population[i].state_dict(), save_folder + "/" + str(iteration) + "_" + str(i))
+        if iteration in model_checkpoint_schedule:
+            for i in range(population_size):
+                torch.save(population[i].state_dict(), save_folder + "/" + str(iteration) + "_" + str(i))
 
-		observation = [[] for i in range(population_size)]
-		action = [[] for i in range(population_size)]
-		reward = [[] for i in range(population_size)]
-		states = [None for i in range(population_size)]
+        new_configs = [{"slots": [random.choice(arm_weapons), random.choice(misc_weapons), random.choice(tail_weapons)]} for i in range(3 * n_arenas // 2)]
+        await app.update_away_team_config(new_configs)
+        await app.update_home_team_config(new_configs)
+        
+        observation = [[] for i in range(population_size)]
+        action = [[] for i in range(population_size)]
+        reward = [[] for i in range(population_size)]
+        states = [None for i in range(population_size)]
 
-		observation_n = await env.reset()
-		with torch.no_grad():
-			while True:
-				action_n = np.zeros((env.n_agents, 5))
-				for i in range(population_size):
-					action_n[league_agent_mappings[i]], states[i] = population[i].get_action(observation_n[league_agent_mappings[i]], states[i])
+        observation_n = await env.reset()
+        with torch.no_grad():
+            while True:
+                action_n = np.zeros((env.n_agents, 5))
+                for i in range(population_size):
+                    action_n[league_agent_mappings[i]], states[i] = population[i].get_action(observation_n[league_agent_mappings[i]], states[i])
 
-				#act in environment and observe the new obervation and reward (done tells you if episode is over)
-				observation_n, reward_n, done_n, _ = await env.step(action_n)
+                #act in environment and observe the new obervation and reward (done tells you if episode is over)
+                observation_n, reward_n, done_n, _ = await env.step(action_n)
 
-				#collect experience data to learn from
-				for i in range(population_size):
-					observation[i].append(observation_n[league_agent_mappings[i]])
-					reward[i].append(reward_n[league_agent_mappings[i]])
-					action[i].append(action_n[league_agent_mappings[i]])
+                #collect experience data to learn from
+                for i in range(population_size):
+                    observation[i].append(observation_n[league_agent_mappings[i]])
+                    reward[i].append(reward_n[league_agent_mappings[i]])
+                    action[i].append(action_n[league_agent_mappings[i]])
 
-				if all(done_n):
-					break
+                if all(done_n):
+                    break
 
-			# reshapes all collected data to [episode num, timestep]
-			for i in range(population_size):
-				observation[i] = np.swapaxes(np.array(observation[i]), 0, 1)
-				reward[i] = np.swapaxes(np.array(reward[i]), 0, 1)
-				action[i] = np.swapaxes(np.array(action[i]), 0, 1)
+            # reshapes all collected data to [episode num, timestep]
+            for i in range(population_size):
+                observation[i] = np.swapaxes(np.array(observation[i]), 0, 1)
+                reward[i] = np.swapaxes(np.array(reward[i]), 0, 1)
+                action[i] = np.swapaxes(np.array(action[i]), 0, 1)
 
-		#learn from experience
-		print("Training League With PPO")
-		for i in range(population_size):
-			print("\nTraining Population Member", i)
-			population[i].update(observation[i], action[i], reward[i])
+        #learn from experience
+        print("Training League With PPO")
+        for i in range(population_size):
+            print("\nTraining Population Member", i)
+            population[i].update(observation[i], action[i], reward[i])
 
-		agent_pbt_ready = [iteration - x >= pbt_min_iterations - 1 for x in last_PBT_update]
-		if any(agent_pbt_ready):
-			cumulative_rewards = np.array(reward).sum((1, 2)).tolist()
-			print("Cumulative rewards per agent:", cumulative_rewards)
+        agent_pbt_ready = [iteration - x >= pbt_min_iterations - 1 for x in last_PBT_update]
+        if any(agent_pbt_ready):
+            cumulative_rewards = np.array(reward).sum((1, 2)).tolist()
+            print("Cumulative rewards per agent:", cumulative_rewards)
 
-			agents_and_rewards = list(zip(population, cumulative_rewards, agent_pbt_ready))
-			updated_agents = pbt_update_bottom(agents_and_rewards, exploit_methods, explore_methods)
+            agents_and_rewards = list(zip(population, cumulative_rewards, agent_pbt_ready))
+            updated_agents = pbt_update_bottom(agents_and_rewards, exploit_methods, explore_methods)
 
-			for updated_agent_index in updated_agents:
-				last_PBT_update[updated_agent_index] = iteration
+            for updated_agent_index in updated_agents:
+                last_PBT_update[updated_agent_index] = iteration
 
-			print("Completed PBT update for {} agents".format(len(updated_agents)))
+            print("Completed PBT update for {} agents".format(len(updated_agents)))
 
-			update_hyperparameter_history(hyperparam_history_save_file, list(zip(population, [x in updated_agents for x in range(len(population))])), iteration)
+            update_hyperparameter_history(hyperparam_history_save_file, list(zip(population, [x in updated_agents for x in range(len(population))])), iteration)
 
 
 async def main():
